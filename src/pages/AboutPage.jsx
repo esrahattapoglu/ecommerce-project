87f6e275fdf8fc06fcfa79db306df9a267761342
esrahattapoglu <esraahattapoglu@gmail.com>
2026-01-28 17:27:48 +0300

    Add TeamPage and AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,5 @@
 const AboutPage = () => {
   return (
-      {/* Sol text content */}
     <div className="w-full min-h-screen flex items-center justify-center bg-gray-50">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-[#252B42] mb-4">
