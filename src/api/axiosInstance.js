85188d67b62a29953cb79ebccec3452e89fbe756
esrahattapoglu <esraahattapoglu@gmail.com>
2026-06-16 11:45:33 +0300

    update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://ecommerce-backend-2-hu14.onrender.com",
+  baseURL: "https://workintech-fe-ecommerce.onrender.com",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
